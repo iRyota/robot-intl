d639011062edfa8b287fe89d5255f6827a7981f1
iRyota <ryota11096@gmail.com>
2017-11-18 16:56:39 +0900

    improve main.py

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from common.network import *
 from common.trainer import *
+from common.optimizer import *
 
-network = Network()
-optimizer = SGD(0.01)
 
-trainer = Trainer()
+net = Network(input_size=784,output_size=10,hidden_layer_size_list=[100,100],activator='ReLU',enable_weight_decay=False,weight_decay_lambda=None)
+opt = SGD(learning_rate=0.01)
+
+trainer = Trainer(network=net, optimizer=opt, x_train, t_train, x_test, t_test, dataset_loops=20, batch_size=100)
 
 trainer.startTraining()
+
+trainer.showAccuracy()
